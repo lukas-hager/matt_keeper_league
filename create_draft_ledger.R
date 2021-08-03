29f34eb04cc9c7288a17b9a1d003d2a6dabc2a8a
Lukas Hager <lghhager@gmail.com>
2021-08-03 15:05:47 -0600

    code to get draft picks

---
+++ create_draft_ledger.R
@@ -0,0 +1,26 @@
+rm(list = ls())
+gc()
+gc()
+
+library(httr)
+library(jsonlite)
+library(tidyverse)
+
+league_id <- '650808198130929664'
+draft_id <- '650808198130929665'
+
+picks <- httr::GET(str_interp('https://api.sleeper.app/v1/draft/${draft_id}/picks')) %>% 
+  httr::content(as = 'text') %>% 
+  jsonlite::fromJSON() 
+
+users <- httr::GET(str_interp('https://api.sleeper.app/v1/league/${league_id}/users')) %>% 
+  httr::content(as = 'text') %>% 
+  jsonlite::fromJSON()
+
+matt_db <- picks %>% 
+  jsonlite::flatten() %>% 
+  select(round, pick_no, picked_by, metadata.first_name, metadata.last_name,
+         player_id) %>% 
+  left_join(users %>% 
+              select(user_id, display_name),
+            by = c('picked_by' = 'user_id'))
